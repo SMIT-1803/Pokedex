10e4cc4b22f63795254a12d208826cdff69d1b55
SMIT-1803 <smitsachin1@gmail.com>
2025-05-02 16:09:36 -0700

    Route setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider  } from 'react-router-dom'
+import PokemonDetails from './components/PokemonDetails.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path = '/' element = {<App/>}>
+        <Route path='pokemon/:name' element={<PokemonDetails />} />
+      </Route>
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
